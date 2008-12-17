60dc6e1445d20c01806a57898b9c108ef79da93f
Christopher Hall <hsw@openmoko.com>
2008-12-17 19:16:49 +0800

    Split out colours and sizes to Theme

--- graphicshell/grshell.py
+++ graphicshell/grshell.py
@@ -13,6 +13,37 @@ import pygame
 from pygame.locals import *
 
 
+class Theme:
+    width, height = 480, 640
+    gap = 10
+
+    class Menu:
+        background = Colour.yellow
+        class Button:
+            foreground = Colour.lightSkyBlue
+            background = Colour.blue
+            perRow = 3
+            perColumn = 7
+
+    class Dialog:
+        x = 50
+        y = 100
+
+    class Status:
+        fontsize = 20
+        lines = 10
+
+        class Default:
+            foreground = Colour.blue
+            background = Colour.grey200
+        class Pass:
+            foreground = Colour.blue
+            background = Colour.paleGreen
+        class Fail:
+            foreground = Colour.red
+            background = Colour.white
+
+
 pygame.display.init()
 pygame.font.init()
 
@@ -21,21 +52,26 @@ testDir = '/etc/test.d'
 backupFile = re.compile('^(.*~|.*\.(old|orig|bak))$', re.IGNORECASE)
 
 
-width, height = 480, 640
-s = Screen('Test shell', width, height)
+s = Screen('Test shell', Theme.width, Theme.height)
 
-dirFrame = Frame("dir", rect = (0, 0, width, height), parent = s, background = Colour.yellow)
+dirFrame = Frame("dir", rect = (0, 0, Theme.width, Theme.height), parent = s, background = Theme.Menu.background)
 
-fontsize = 20
-tOffset = 10
-tWidth = width - 2 * tOffset
-tHeight = 8 * fontsize
-tVertical = height - tHeight - 10
-status = Text('', fontsize = fontsize, rect = (tOffset, tVertical, tWidth, tHeight), parent = s)
+# the gaps bwtween everything
 
+tWidth = Theme.width - 2 * Theme.gap
+tHeight = Theme.Status.lines * Theme.Status.fontsize
+tVertical = Theme.height - tHeight - Theme.gap
+status = Text('', fontsize = Theme.Status.fontsize, \
+                  rect = (Theme.gap, tVertical, tWidth, tHeight), parent = s, \
+                  background = Theme.Status.Default.background, \
+                  foreground = Theme.Status.Default.foreground, \
+                  )
+
+status.addTag('FAIL', Theme.Status.Fail.foreground, Theme.Status.Fail.background)
+status.addTag('PASS', Theme.Status.Pass.foreground, Theme.Status.Pass.background)
 
 def request(prompt):
-    dialog = Dialog(prompt + "\n\n", 50, 100, s)
+    dialog = Dialog(prompt + "\n \n", Theme.Dialog.x, Theme.Dialog.y, s)
     dialog.run()
     return dialog.state
 
@@ -43,10 +79,11 @@ def request(prompt):
 programList = []
 buttonList = []
 
-offset = 10
-(buttonX, buttonY, buttonW, buttonH) = (offset, offset, 140, 50)
-across = buttonW + offset
-down = buttonH + offset
+buttonW = (Theme.width - (Theme.Menu.Button.perRow + 1) * Theme.gap) / Theme.Menu.Button.perRow
+buttonH = (Theme.height - (Theme.Menu.Button.perColumn + 2) * Theme.gap - tHeight) / Theme.Menu.Button.perColumn
+(buttonX, buttonY) = (Theme.gap, Theme.gap)
+across = buttonW + Theme.gap
+down = buttonH + Theme.gap
 
 def runProgram(p):
     status.draw()
@@ -63,11 +100,13 @@ for f in os.listdir(testDir):
             if p != None and p.runnable:
                 programList.append(p)
                 b = Button(p.menu, rect = (buttonX, buttonY, buttonW, buttonH), \
+                               background = Theme.Menu.Button.background, \
+                               foreground = Theme.Menu.Button.foreground, \
                                parent = dirFrame, callback = runProgram, callbackarg = p)
                 buttonList.append(b)
                 buttonX += across
-                if buttonX + buttonW > width:
-                    buttonX = offset
+                if buttonX + buttonW > Theme.width:
+                    buttonX = Theme.gap
                     buttonY += down
 
 eventHandler([dirFrame, status])
