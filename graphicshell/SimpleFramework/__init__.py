6e6a362a5531db4f6590d09be1e89c6f586168bd
Christopher Hall <hsw@openmoko.com>
2008-12-19 11:39:14 +0800

    move the framework to its own module

---
+++ graphicshell/SimpleFramework/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# COPYRIGHT: Openmoko Inc. 2008
+# LICENSE: GPL Version 2 or later
+# DESCRIPTION: Simple frame and dialog box framework
+
+from SimpleFramework.framework import *
+from SimpleFramework.colour import Colour
