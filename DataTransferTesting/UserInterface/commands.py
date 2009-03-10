a2fe7fb4e425b22de35e1538b8ebdf9b395e61e9
Christopher Hall <hsw@openmoko.com>
2009-03-10 10:16:28 +0800

    Improve the getIP command so there is no need for awk and add some more comments to doc strings.

--- DataTransferTesting/UserInterface/commands.py
+++ DataTransferTesting/UserInterface/commands.py
@@ -14,14 +14,15 @@ import pexpect
 
 
 def local(command, input = None):
-    """run a command using the shell"""
-    #print 'running:', command
+    """run a command using the shell
+
+    Just captures all output and error
+    and returns them as strings, along with the exit code
+    """
     proc = subprocess.Popen(['sh', '-c', command], stdin = subprocess.PIPE,
                             stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 
     (output, error) = proc.communicate(input)
-    #print 'O:', output
-    #print 'E:', error
     output = output.strip('\r\n \t')
     error = error.strip('\r\n \t')
     
@@ -29,14 +30,17 @@ def local(command, input = None):
 
 
 def remote(ip, command):
-    """Run a command on a remote device ove ssh link"""
+    """Run a command on a remote device over ssh link
+
+    Just captures all output and error
+    and returns them as strings, along with the exit code
+    """
     ssh_command = 'ssh root@' + ip + ' ' + command
-    #print 'run:', ssh_command
+
     proc = pexpect.spawn(ssh_command)
 
     while True:
         rc = proc.expect([r'(?i)password:', r'\(yes/no\)\?', pexpect.TIMEOUT])
-        #print 'D:', proc.before
         if rc == 0:
             proc.sendline('')
             proc.expect(pexpect.EOF)
@@ -51,21 +55,30 @@ def remote(ip, command):
 
 def getIP(ether):
     """Return the IP address of a network device"""
-    (output, error, rc) = local('ifconfig ' + ether + """ | awk r'
-                      /inet[[:space:]]+addr:/{
-                         x = $2
-                         gsub("addr:", "", x)
-                         print x
-                      }'
-    """)
-    if rc == 0 and output != '':
-        return (True, output)
-    return (False, 'Interface is not set up')
+    proc = pexpect.spawn('ifconfig ' + ether)
+
+    result = False
+    ip = 'Interface is not set up'
+    while True:
+        rc = proc.expect(['inet\s+addr:\s*([0-9.]+)\s', pexpect.EOF, pexpect.TIMEOUT])
+        if rc == 0:
+            ip = proc.match.group(1)
+            result = True
+        elif rc == 1:
+            break
+        else:
+            proc.terminate()
+            time.sleep(2)
+            proc.kill(9)
+            break
+    rc = proc.exitstatus
+    proc.close()
+    return (result, ip)
 
 
 def ping(hostname):
     """ ping a host or IP address and return status"""
-    (output, error, rc) = local('ping -c 2 -i 0.4 -w 2 ' + hostname)
+    (output, error, rc) = local('ping -c 2 -i 0.4 -w 2 "' + hostname + '"')
     if rc == 0:
         return (True, output)
     return (False, error)
@@ -73,43 +86,28 @@ def ping(hostname):
 
 def sendOneFile(hostname, privateKey, fromFile, toFile, callback):
     """Send a local file to remote using scp"""
-    command = 'scp -p -o "Compression no" -i ' + privateKey + ' ' + fromFile + ' root@' + hostname + ':' + toFile
-    #(output, error, rc) = local(command)
-    #if output == '':
-    #    output = 'OK'
-    #if rc == 0:
-    #    return (True, output, command)
-    #return (False, error, command)
+    command = 'scp -p -o "Compression no" -i "' + privateKey + '" "' + fromFile + '" "root@' + hostname + ':' + toFile + '"'
     rc = runWithCallback(command, callback)
     return (rc == 0, str(rc), command)
 
 def receiveOneFile(hostname, privateKey, fromFile, toFile, callback):
     """Retrieve a remote file using scp"""
-    command = 'scp -p  -i ' + privateKey + ' root@' + hostname + ':' + fromFile + ' ' + toFile
-    #(output, error, rc) = local(command)
-    #if output == '':
-    #    output = 'OK'
-    #if rc == 0:
-    #    return (True, output, command)
-    #return (False, error, command)
+    command = 'scp -p -o "Compression no" -i "' + privateKey + '" "root@' + hostname + ':' + fromFile + '" "' + toFile + '"'
     rc = runWithCallback(command, callback)
     return (rc == 0, str(rc), command)
 
 
 def sendAuth(hostname, publicKey):
     """Copy public key to remote ssh configuration"""
-    command = 'scp ' + publicKey + ' root@' + hostname + ':.ssh/authorized_keys'
-    #print 'run:', command
+    command = 'scp "' + publicKey + '" "root@' + hostname + ':.ssh/authorized_keys"'
     proc = pexpect.spawn(command)
 
     while True:
         rc = proc.expect([r'(?i)password:', r'\(yes/no\)\?', pexpect.TIMEOUT])
-        #print 'D:', proc.before
         if rc == 0:
             proc.sendline('')
             proc.expect(pexpect.EOF)
             proc.close()
-            #print 'X:', proc.exitstatus, '\nY:', proc.before
             return (True, 'Authentication files transferred')
         elif rc == 1:
             proc.sendline('yes')
@@ -119,25 +117,32 @@ def sendAuth(hostname, publicKey):
     return (False, 'Send public key failed')
 
 
-def runWithCallback(command, callback):
-    """Run a local program calling back with every line of output"""
-    #print 'run:', command
+def runWithCallback(command, callback = None):
+    """Run a local program calling back with every line of output
+
+    The command is run and each line of stdout is passed to the callback
+    the callback returns True to get the next line of output
+    or False to terminate the process (uses kill, then kill -9)
+    """
     proc = pexpect.spawn(command)
+    output = ''
 
     while True:
-        rc = proc.expect(['([^\n\r]*)[\r\n]', pexpect.EOF, pexpect.TIMEOUT])
-        #print 'D:', proc.before
+        rc = proc.expect(['([^\n\r]*)(\r*\n|\n*\r)', pexpect.EOF, pexpect.TIMEOUT])
         if rc == 0:
-            if not callback(proc.match.group(1)):
-                #print "*TERMINATE*"
+            if callback == None:
+                pass
+            elif not callback(proc.match.group(1)):
                 proc.terminate()
                 time.sleep(2)
                 proc.kill(9)
-                #proc.wait() # cannot wait if already dead
                 break
         elif rc == 1:
             break
         else:
+            proc.terminate()
+            time.sleep(2)
+            proc.kill(9)
             break
     rc = proc.exitstatus
     proc.close()
@@ -163,11 +168,15 @@ if __name__ == "__main__":
     print '\n-----\nerror:\n', err
     print '\n-----\n'
 
-    print 'RWC:'
+    print 'Run With Callback:'
     i = 0
     def pl(s):
         global i
         i += 1
-        print i, ':', s
-    runWithCallback('ls -l', pl)
+        print 'RWC', i, ':', s
+        return True
+    runWithCallback('ls -l /boot', pl)
+    print '\n-----\n'
+    print 'eth0 =', getIP('eth0')
+    print 'eth1 =', getIP('eth1')
     print '\n-----\n'
