db467fc28ffbfcad116becadeef09acf112254cd
Christopher Hall <hsw@openmoko.com>
2009-05-11 10:58:21 +0800

    Test for the relay driver
    Signed-off-by: Christopher Hall <hsw@openmoko.com>

--- control-program/RelayBoard.py
+++ control-program/RelayBoard.py
@@ -11,41 +11,51 @@ from time import sleep
 
 class RelayBoard():
 
-    def __init__(self, port = '/dev/ttyUSB0', bps = 19200):
+    def __init__(self, port = '/dev/ttyUSB1', bps = 19200):
 
         self.relay = Serial(port = port, baudrate = bps)
         self.relay.open()
         self.allOff()
 
-
     def __del__(self):
         self.relay.close()
-
 
     def allOff(self):
         self.map = 0
         self.update()
 
-
     def update(self):
         print '{%04x}' % (self.map & 0xffff)
+        self.relay.write('{%04x}' % (self.map & 0xffff))
 
+    def state(self):
+        return self.map
 
-    @property
-    def on(self):
-        return 0
-
-
-    @on.setter
-    def on(self, n):
+    def set(self, n):
         if 1 > n or 16 < n:
             return
-        map |= 1 << (n - 1)
-        self.update()
+        self.map |= 1 << (n - 1)
+
+    def clear(self, n):
+        if 1 > n or 16 < n:
+            return
+        self.map &= ~(1 << (n - 1))
 
 def main():
     r = RelayBoard()
-    r.on = 4
+    r.set(4)
+    r.set(7)
+    r.update()
+    sleep(0.25)
+    r.allOff()
+    sleep(0.1)
+
+    for i in range(1, 17):
+      r.set(i)
+      r.update()
+      sleep(0.1)
+      r.clear(i)
+
     r = None
 
 if __name__ == '__main__':
