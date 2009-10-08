335abe8014507565cfa30a7c8bd8fb76217dbd8c
Christopher Hall <hsw@openmoko.com>
2009-10-08 11:55:54 +0800

    [control-program] updates to relay board module
    Ensure proper set up of the serial port
    
    add on/off methods that action the current item
    and flush all pending changes
    
    Signed-off-by: Christopher Hall <hsw@openmoko.com>

--- control-program/RelayBoard.py
+++ control-program/RelayBoard.py
@@ -11,9 +11,13 @@ from time import sleep
 
 class RelayBoard():
 
-    def __init__(self, port = '/dev/ttyUSB1', bps = 19200):
-
-        self.relay = Serial(port = port, baudrate = bps)
+    def __init__(self, port = '/dev/ttyUSB0', bps = 19200, timeout = 0.1):
+        self.relay = Serial(port = port)
+        self.relay.xonxoff = False
+        self.relay.rtscts = False
+        self.relay.baudrate = bps
+        self.relay.timeout = timeout
+        print self.relay.baudrate, self.relay.bytesize, self.relay.parity, self.relay.stopbits
         self.relay.open()
         self.allOff()
 
@@ -21,25 +25,51 @@ class RelayBoard():
         self.relay.close()
 
     def allOff(self):
+        """Swich off all realays immediately"""
+        self.now = 0
         self.map = 0
         self.update()
 
     def update(self):
-        print '{%04x}' % (self.map & 0xffff)
-        self.relay.write('{%04x}' % (self.map & 0xffff))
+        """Set relay pending state to current state"""
+        s = '{%04x}' % (self.map & 0xffff)
+        self.now = self.map
+        for c in s:
+            self.relay.write(c)
+            while 0 == self.relay.inWaiting():
+                sleep(0.001)
+            self.relay.read(1)
 
     def state(self):
+        """Return the state of the relays"""
+        return self.now
+
+    def pending(self):
+        """Return the pending state of the relays"""
         return self.map
 
     def set(self, n):
+        """Prepare to turn relay on"""
         if 1 > n or 16 < n:
             return
         self.map |= 1 << (n - 1)
 
     def clear(self, n):
+        """Prepare to turn relay off"""
         if 1 > n or 16 < n:
             return
         self.map &= ~(1 << (n - 1))
+
+    def on(self, n):
+        """Turn relay on (and update pending relays)"""
+        self.set(n):
+        self.update();
+
+    def off(self, n):
+        """Turn relay off (and update pending relays)"""
+        self.clear(n):
+        self.update();
+
 
 def main():
     r = RelayBoard()
@@ -50,11 +80,12 @@ def main():
     r.allOff()
     sleep(0.1)
 
-    for i in range(1, 17):
-      r.set(i)
-      r.update()
-      sleep(0.1)
-      r.clear(i)
+    for x in range(5):
+        for i in range(1, 17):
+            r.set(i)
+            r.update()
+            sleep(0.02)
+            r.clear(i)
 
     r = None
 
